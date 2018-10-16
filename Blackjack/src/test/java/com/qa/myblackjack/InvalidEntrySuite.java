242c546a905b0ab8e02a92798ba5611a542ea3f6
vinuruban <vinu_06@hotmail.com>
2018-10-16 10:05:30 +0100

    My first commit

---
+++ Blackjack/src/test/java/com/qa/myblackjack/InvalidEntrySuite.java
@@ -0,0 +1,12 @@
+package com.qa.myblackjack;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+
+@Suite.SuiteClasses({invalidTest.class,bustTest.class})
+
+public class InvalidEntrySuite {
+
+}
